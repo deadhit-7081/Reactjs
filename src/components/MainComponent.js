b01febc55efdd52cebd111da1ef7f563d5b88333
Jai <jaikumarsingh1908@gmail.com>
2020-04-02 17:46:55 +0530

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import {Navbar, NavbarBrand } from 'reactstrap';
 import DishDetail from './DishDetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
 
 class Main extends Component {
@@ -25,14 +26,11 @@ class Main extends Component {
     }
   render () {
   return (
-    <div className="App">
-      <Navbar dark color="primary">
-      <div class="container">
-        <NavbarBrand href="/">Webhit</NavbarBrand>
-      </div>
-      </Navbar>
+    <div>
+      <Header/>
       <Menu jai={this.state.dishes} clk={(dishId) => this.onDishSelect(dishId)}/>
       <DishDetail ddetail={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+      <Footer/>
     </div>
   );
   }
